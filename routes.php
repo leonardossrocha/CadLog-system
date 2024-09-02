9eb499c21e1e9c81e3cf4746d8119cec24e4a719
Leonardo Rocha <leonardo.sidon@grupomarista.org.br>
2024-09-02 11:50:39 -0300

    implementa rotas e configura arquivo index

---
+++ routes.php
@@ -0,0 +1,40 @@
+<?php
+require 'controllers/AuthController.php';
+require 'controllers/UserController.php';
+require 'controllers/DashboardController.php';
+
+$authController = new AuthController();
+$userController = new UserController();
+$dashboardController = new DashboardController();
+
+$action = $_GET['action'] ?? 'login';
+
+switch ($action) {
+    case 'login':
+        $authController->login();
+        break;
+    case 'logout':
+        $authController->logout();
+        break;
+    case 'register':
+        $userController->register();
+        break;
+    case 'dashboard':
+        $dashboardController->index();
+        break;
+    case 'list':
+        $userController->list();
+        break;
+    case 'edit':
+        $id = $_GET['id'];
+        $userController->edit($id);
+        break;
+    case 'delete':
+        $id = $_GET['id'];
+        $userController->delete($id);
+        break;
+    default:
+        $authController->login();
+        break;
+}
+?>
