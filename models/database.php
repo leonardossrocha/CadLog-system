20ef3d528ce33cd50938d5cf5fd4b70cb962060c
Leonardo Rocha <leonardo.sidon@grupomarista.org.br>
2024-09-02 11:15:44 -0300

    adiciona código nos arquivos de modelo

---
+++ models/database.php
@@ -0,0 +1,18 @@
+<?php
+class Database {
+    private static $instance = null;
+
+    public static function getConnection() {
+        if (!self::$instance) {
+            $host = 'localhost';
+            $db = 'sistema_usuarios';
+            $user = 'root';
+            $password = '';
+
+            self::$instance = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        return self::$instance;
+    }
+}
+?>
