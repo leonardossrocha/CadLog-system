95c952300b087b4325eb61f5f98b6e5a601d245a
Leonardo Rocha <leonardossr@gmail.com>
2025-05-03 08:52:52 -0300

    adiciona comentário explicativo sobre a função do arquivo

--- auth.php
+++ auth.php
@@ -1,4 +1,5 @@
 <?php
+// Arquivo middleware -  camada intermediária entre a requisição do usuário e a lógica principal da aplicação que serve para interceptar e processar a requisição antes que ela continue. Em sistemas com autenticação é usado principalmente para proteger páginas que só devem ser acessadas por usuários logados ou com certos perfis.
 session_start(); // Inicia uma nova sessão ou retoma a sessão atual. Isso é necessário para acessar variáveis de sessão.
 
 if (!isset($_SESSION['usuario_id'])) { // Verifica se a variável de sessão 'usuario_id' está definida.
